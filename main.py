46ea48cbf9205771fb922a7cd284c84490dd92eb
Erika Wernbro <155359298+Tejpex@users.noreply.github.com>
2024-12-07 10:25:56 +0100

    Add db-address

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
